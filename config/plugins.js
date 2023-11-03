7ec54b8bae4caf87c3aecd76930a939599f074e0
Ethan Cook <ethan@dealervenom.com>
2023-11-03 11:49:05 -0400

    feat(plugins): Installed Plugins

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  redirects: {
+    enabled: true
+  },
+  tinymce: {
+    enabled: true
+  },
+  'drag-drop-content-types': {
+    enabled: true
+  },
+  seo: {
+    enabled: true
+  }
+})
